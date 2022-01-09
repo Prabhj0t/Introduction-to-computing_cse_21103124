fbc7656028d449444812bac54778bcbca5194bb9
Prabhj0t <97307294+Prabhj0t@users.noreply.github.com>
2022-01-09 23:08:16 +0530

    Add files via upload

---
+++ asg1_outputfile_cse_21103124.py
@@ -0,0 +1,38 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Prabhjot Singh/Desktop/semester 1/Introduction to computing (ESC 1L)/computing idle/asg1_21103124/first 1.py
+enter the value of num1:40
+enter the value of num2:34
+enter the value of num3:33
+the Average of three numbers you entered is : 35.666666666666664
+
+= RESTART: C:/Users/Prabhjot Singh/Desktop/semester 1/Introduction to computing (ESC 1L)/computing idle/asg1_21103124/second 2.py
+Enter your Gross income: 150000
+enter the number of dependents: 3
+The Final Tax amount should be paid is:  26200.0
+
+= RESTART: C:/Users/Prabhjot Singh/Desktop/semester 1/Introduction to computing (ESC 1L)/computing idle/asg1_21103124/third 3.py
+Enter your StudentID:21103124
+enter your Name:Prabhjot Singh
+enter your Gender M/F/U:M
+enter your course you studing:CSE
+enter your CGPA:8.9
+
+***STUDENT DETAIL***
+[21103124, 'Prabhjot Singh', 'Male', 'CSE', 8.9]
+
+= RESTART: C:/Users/Prabhjot Singh/Desktop/semester 1/Introduction to computing (ESC 1L)/computing idle/asg1_21103124/fourth 4.py
+Marks of 1st student : 24
+Marks of 2nd student : 22
+Marks of 3rd student : 17
+Marks of 4th student : 16
+Marks of 5th student : 30
+[16, 17, 22, 24, 30]
+
+= RESTART: C:/Users/Prabhjot Singh/Desktop/semester 1/Introduction to computing (ESC 1L)/computing idle/asg1_21103124/fifth 5.py
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+output of part a is:
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+output of part b is:
+['Red', 'Green', 'White', 'Purple', 'Yellow']
