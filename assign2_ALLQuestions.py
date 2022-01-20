9900524e0258a5071abf85816df8aa68b92bf23b
Prabhj0t <97307294+Prabhj0t@users.noreply.github.com>
2022-01-20 17:31:03 +0530

    Add files via upload

---
+++ assign2_ALLQuestions.py
@@ -0,0 +1,95 @@
+# ASSIGNMENT-2
+# ques1
+
+# take input "Python is a case sensitive language"
+input_string = input("Enter the string:")
+# a
+print("Part A:")
+print("Length of string is:",len(input_string))
+
+# b
+print("Part B:")
+print("reverse of string is:","'",input_string[::-1],"'")
+
+# c
+print("Part C:")
+# "a case senstive" has index value 9 to 26
+new_string = input_string[slice(9, 26)]
+print("new string is:","'",new_string,"'")
+
+# d
+print("Part D:")
+replaced_string = input_string.replace("a case sensitive", "object oriented")
+print(replaced_string)
+
+# e
+print("Part E:")
+print("Index of 'a' in given input string is:", input_string.index('a'))
+
+# f
+print("Part F:")
+print(input_string.replace(" ", ""))
+
+# ques2
+Name = input("Enter your name:")
+SID = int(input("Enter you SID:"))
+DEPARTMENT = input("Enter your department:")
+CGPA = float(input("Enter your CGPA:"))
+print("Hey,%s Here! \n My SID is %d \nI am from %s department and my CGPA is %.2f" %
+      (Name, SID, DEPARTMENT, CGPA))
+
+# ques3
+# given a=56 and b=10 using bitwise operators
+a = 56
+b = 10
+print("a=56\nb=10")
+print("Part A:")
+print("a&b: ", a & b)
+print("Part B:")
+print("a|b: ", a | b)
+print("Part C:")
+print("a^b: ", a ^ b)
+print("Part D:")
+print("a<<2:", a << 2, "and b<<2", b << 2)
+print("Part E:")
+print("a>>2:", a >> 2, "and b>>4", b >> 4)
+
+# ques4
+# first taking the input by user of three no.s
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# applying conditional
+if (num1 > num2) and (num1 > num3):
+    largest = num1
+elif (num2 > num1) and (num2 > num3):
+    largest = num2
+else:
+    largest = num3
+
+print("The largest number is", largest)
+
+# ques5
+# first taking input string by user
+input_string = input("Enter the string: ")
+if ("name" in input_string):
+    print("Yes, 'name' is present in this string")
+else:
+    print("No, 'name' is not present in this string")
+
+
+# ques6
+# define the sides of a triangle with variable a,b and c.
+side1 = input("Enter the value of side a:")
+side2 = input("Enter the value of side b:")
+side3 = input("Enter the value of side c:")
+# using conditional statment
+# if any side is length 0 not possible a triangle
+if (int(side1) == 0) or (int(side2) == 0) or (int(side3) == 0):
+    print("No")
+# any of three side shouldn't greater than the some of other two sides
+elif (int(side3) > (int(side1)+int(side2))) or (int(side2) > (int(side1)+int(side3))) or (int(side1) > (int(side2)+int(side3))):
+    print("No")
+else:
+    print("Yes")
---
+++ output_assign2.py
@@ -0,0 +1,68 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:\Users\Prabhjot Singh\Desktop\assgnment2\assign2_1stq.py =====
+Enter the string:Python is a case sensitive language
+Part A:
+Length of string is: 35
+Part B:
+reverse of string is: ' egaugnal evitisnes esac a si nohtyP '
+Part C:
+new string is : '  a case sensitive '
+Part D:
+Python is object oriented language
+Part E:
+Index of 'a' in given input string is: 10
+Part F:
+Pythonisacasesensitivelanguage
+
+====== RESTART: C:\Users\Prabhjot Singh\Desktop\assgnment2\assign2_2ndq.py =====
+Enter your name:Prabhjot Singh
+Enter you SID:21103124
+Enter your department:CSE
+Enter your CGPA:9.9
+Hey,Prabhjot Singh Here! 
+ My SID is 21103124 
+I am from CSE department and my CGPA is 9.90
+
+====== RESTART: C:\Users\Prabhjot Singh\Desktop\assgnment2\assign2_3rdq.py =====
+a=56
+b=10
+Part A:
+a&b:  8
+Part B:
+a|b:  58
+Part C:
+a^b:  50
+Part D:
+a<<2: 224 and b<<2 40
+Part E:
+a>>2: 14 and b>>4 0
+
+====== RESTART: C:\Users\Prabhjot Singh\Desktop\assgnment2\assign2_4thq.py =====
+Enter first number: 75
+Enter second number: 87
+Enter third number: 45
+The largest number is 87.0
+
+====== RESTART: C:\Users\Prabhjot Singh\Desktop\assgnment2\assign2_5thq.py =====
+Enter the string: hi!,tell me your name 
+Yes, 'name' is present in the string
+
+====== RESTART: C:\Users\Prabhjot Singh\Desktop\assgnment2\assign2_5thq.py =====
+Enter the string: hi!,everybody
+No,'name' is not present in the string
+
+
+=========== RESTART: C:/Users/Prabhjot Singh/Desktop/assign2_6thq.py ===========
+Enter the length of side a:3
+Enter the length of side b:5
+Enter the length of side c:4
+Yes
+
+
+=========== RESTART: C:/Users/Prabhjot Singh/Desktop/assign2_6thq.py ===========
+Enter the length of side a:34
+Enter the length of side b:2
+Enter the length of side c:18
+No
