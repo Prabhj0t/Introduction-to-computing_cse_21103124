94d6c36e2dc51729b1e3fe69631622facb637bd8
Prabhj0t <97307294+Prabhj0t@users.noreply.github.com>
2022-01-24 14:09:21 +0530

    Update assign2_ALLQuestions.py

--- assign2_ALLQuestions.py
+++ assign2_ALLQuestions.py
@@ -61,9 +61,9 @@ num2 = float(input("Enter second number: "))
 num3 = float(input("Enter third number: "))
 
 # applying conditional
-if (num1 > num2) and (num1 > num3):
+if (num1 >= num2) and (num1 >= num3):
     largest = num1
-elif (num2 > num1) and (num2 > num3):
+elif (num2 >= num1) and (num2 >= num3):
     largest = num2
 else:
     largest = num3
